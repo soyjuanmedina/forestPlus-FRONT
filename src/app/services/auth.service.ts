ad2ea5b275f98a152fb08ba2ae2bf27517cdd845
SERES\juan.medina <juan.medina@seres.es>
2025-09-22 13:24:46 +0200

    responsive menu

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable( { providedIn: 'root' } )
+export class AuthService {
+  private loggedIn = new BehaviorSubject<boolean>( false );
+  isLoggedIn$ = this.loggedIn.asObservable();
+
+  login () { this.loggedIn.next( true ); }
+  logout () { this.loggedIn.next( false ); }
+
+  get isLoggedIn () { return this.loggedIn.value; }
+}
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
-import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component( {
@@ -8,14 +9,23 @@ import { TranslateModule } from '@ngx-translate/core';
   standalone: true,
   imports: [TranslateModule, CommonModule, RouterModule],
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss'],
+  styleUrls: ['./sidebar.component.scss']
 } )
 export class SidebarComponent {
-  @Input() sidebarOpen = false;          // Estado del sidebar (mobile)
+  @Input() sidebarOpen = false;                // estado desde el padre
   @Output() sidebarOpenChange = new EventEmitter<boolean>();
+
+  constructor ( public auth: AuthService ) { }
 
   toggleSidebar () {
     this.sidebarOpen = !this.sidebarOpen;
     this.sidebarOpenChange.emit( this.sidebarOpen );
+  }
+
+  closeSidebar () {
+    if ( this.sidebarOpen ) {
+      this.sidebarOpen = false;
+      this.sidebarOpenChange.emit( this.sidebarOpen );
+    }
   }
 }
