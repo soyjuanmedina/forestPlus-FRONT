9e4b1e30ba0005d098047fb33be07aa2812e797c
SERES\juan.medina <juan.medina@seres.es>
2025-10-02 18:45:25 +0200

    New enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://forestplus-back-production.up.railway.app/api'
+  //apiBaseUrl: 'https://forestplus-back-production.up.railway.app/api'
+  apiBaseUrl: 'https://mi-app-143810247129.europe-southwest1.run.app'
 };
