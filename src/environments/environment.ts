765810fc2d6c203d65f191704b1de4b58e800e2e
Juan Medina <soyjuanmedina@gmail.com>
2025-10-05 18:08:37 +0200

    Open Api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   //apiBaseUrl: 'https://forestplus-back-production.up.railway.app/api'
-  apiBaseUrl: 'https://mi-app-143810247129.europe-southwest1.run.app'
+  //apiBaseUrl: 'https://mi-app-143810247129.europe-southwest1.run.app/api'
+  apiBaseUrl: 'https://forestplus-back-143810247129.europe-southwest1.run.app/api'
 };
