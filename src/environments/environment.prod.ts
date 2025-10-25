1db2a98a5a3d7b19d29747a3cec2e32d126687b8
SERES\juan.medina <juan.medina@seres.es>
2025-10-25 12:21:04 +0200

    forestplusapp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   //apiBaseUrl: 'https://forestplus-back-production.up.railway.app'
   //apiBaseUrl: 'https://mi-app-143810247129.europe-southwest1.run.app'
-  apiBaseUrl: '/'
+  apiBaseUrl: 'https://forestplusapp.com'
 };
