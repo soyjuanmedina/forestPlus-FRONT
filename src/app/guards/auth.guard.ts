11016a64144c10208da8eea161c7f05675121280
SERES\juan.medina <juan.medina@seres.es>
2025-10-25 19:45:29 +0200

    New canActivate

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -10,13 +10,14 @@ export class AuthGuard implements CanActivate {
 
   constructor ( private authService: AuthService, private router: Router ) { }
 
-  canActivate (): boolean {
-    const user = this.authService.getUser(); // devuelve UserResponseDto | null
-
-    console.log( 'user', user );
-    if ( user ) return true;
-
-    this.router.navigate( ['/login'] );
-    return false;
+  canActivate () {
+    return this.authService.user$.pipe(
+      take( 1 ),
+      map( user => {
+        if ( user ) return true;
+        this.router.navigate( ['/login'] );
+        return false;
+      } )
+    );
   }
 }
