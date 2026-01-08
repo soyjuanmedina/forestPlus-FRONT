44bffde1fb84f195a13580d86f1b693865344598
SERES\juan.medina <juan.medina@seres.es>
2026-01-08 16:58:55 +0100

    provideApi

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { environment } from '../environments/environment';
+import { provideApi } from './api';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection( { eventCoalescing: true } ),
+  provideRouter( routes ),
+  provideAnimationsAsync(),
+  provideAnimationsAsync(),
+  provideApi( environment.apiBaseUrl )
+  ]
 };
